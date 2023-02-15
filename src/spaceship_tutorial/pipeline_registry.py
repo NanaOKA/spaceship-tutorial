c4d8cae16f66b39ca6c29a347f5af5f617e250a4
abanan <nana.abankwa@codeworldwide.com>
2023-02-15 23:47:59 +0000

    Setting default pipelines as data_science + data_processing ahead of tutorial on experiment tracking

--- src/spaceship_tutorial/pipeline_registry.py
+++ src/spaceship_tutorial/pipeline_registry.py
@@ -4,6 +4,10 @@ from typing import Dict
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+from tutorial_template.pipelines import (
+    data_processing,
+    data_science
+)
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -11,6 +15,15 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    # pipelines = find_pipelines()
+    # pipelines["__default__"] = sum(pipelines.values())
+    # return pipelines
+
+    data_science_pipeline = data_science.create_pipeline()
+    data_processing_pipeline = data_processing.create_pipeline()
+
+    return {
+        "__default__": data_science_pipeline + data_processing_pipeline,
+        "data_science_pipeline": data_science_pipeline,
+        "data_processing_pipeline": data_processing_pipeline,
+    }
